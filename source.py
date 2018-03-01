2c55a4bc1224c6273cf0e9f68addc002b3d0365c
Remi <remigiuszgebka@gmail.com>
2018-03-01 19:54:27 +0100

    take best
    Signed-off-by: Remi <remigiuszgebka@gmail.com>

--- source.py
+++ source.py
@@ -20,10 +20,9 @@ class ride:
 
 testCars = list()
 testRides = list()
-def takeBestRide(x, y):
-    #najnizszy czas startu
-    #najniszy czas konca
-    #najmniejszy dystans
+
+
+def take_best_ride(x, y):
     act_best_ride_id = testRides[0].ride_id
     act_best_start = testRides[0].start_time
     act_best_finish = testRides[0].end_time
@@ -31,7 +30,7 @@ def takeBestRide(x, y):
     for element in testRides:
         if element.start_time <= act_best_start:
             if element.end_time <= act_best_finish:
-                if act_best_distance_to_start <= abs( x - element.start_x) + abs( y - element.start_y):
+                if act_best_distance_to_start <= abs(x - element.start_x) + abs(y - element.start_y):
                     act_best_ride_id = element.ride_id
     return act_best_ride_id
 
