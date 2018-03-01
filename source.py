86c5c9645720a762e2e84710785a56edcfe5695c
Remi <remigiuszgebka@gmail.com>
2018-03-01 20:06:18 +0100

    points
    Signed-off-by: Remi <remigiuszgebka@gmail.com>

--- source.py
+++ source.py
@@ -1,9 +1,13 @@
+import fileservice as filer
+
+
 class car:
     def __init__(self, x, y, ids, ava):
         self.actual_x = x
         self.actual_y = y
         self.car_id = ids
         self.ava_time = ava
+        self.rides = list()
 
 
 class ride:
@@ -18,20 +22,25 @@ class ride:
         self.distance = (abs(self.end_x-self.start_x) + abs(self.end_y - self.start_y))
 
 
-testCars = list()
-testRides = list()
-
+testCars = filer.get_cars()
+testRides = filer.get_ride_list()
+totalPoints = 0
 
 def take_best_ride(x, y):
     act_best_ride_id = testRides[0].ride_id
     act_best_start = testRides[0].start_time
     act_best_finish = testRides[0].end_time
-    act_best_distance_to_start = abs( x - testRides[0].start_x) + abs( y - testRides[0].start_y)
+    act_best_distance_to_start = abs(x - testRides[0].start_x) + abs(y - testRides[0].start_y)
     for element in testRides:
         if element.start_time <= act_best_start:
             if element.end_time <= act_best_finish:
                 if act_best_distance_to_start <= abs(x - element.start_x) + abs(y - element.start_y):
                     act_best_ride_id = element.ride_id
     return act_best_ride_id
+
+#punktowanie: czas na miejscu +B pkt, przejazd w czasie dystans
+#parametry stopu: kroki, koniec przejazdow
+
+
 
 
