37d9189cea81bc303630ab0f724049d3ad965f55
Remi <remigiuszgebka@gmail.com>
2018-03-01 19:33:34 +0100

    class up
    Signed-off-by: Remi <remigiuszgebka@gmail.com>

--- source.py
+++ source.py
@@ -1,17 +1,18 @@
 class car:
-    def __init__(self, x, y, id, ava):
+    def __init__(self, x, y, ids, ava):
         self.actual_x = x
         self.actual_y = y
-        self.car_id = id
+        self.car_id = ids
         self.ava_time = ava
 
 class ride:
-    def __init__(self, s_x, s_y, e_x, e_y, s_time, f_time, id):
+    def __init__(self, s_x, s_y, e_x, e_y, s_time, f_time, ids):
         self.start_x = s_x
         self.start_y = s_y
         self.end_x = e_x
         self.end_y = e_y
         self.start_time = s_time
         self.end_time = f_time
+        self.ride_id = ids
         self.distance = (abs(self.end_x-self.start_x) + abs(self.end_y - self.start_y))
 
