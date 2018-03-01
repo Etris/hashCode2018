93596f834a9259d809e1a1820af3f97e935a2738
Remi <remigiuszgebka@gmail.com>
2018-03-01 19:52:32 +0100

    take best
    Signed-off-by: Remi <remigiuszgebka@gmail.com>

--- source.py
+++ source.py
@@ -5,6 +5,7 @@ class car:
         self.car_id = ids
         self.ava_time = ava
 
+
 class ride:
     def __init__(self, s_x, s_y, e_x, e_y, s_time, f_time, ids):
         self.start_x = s_x
@@ -15,4 +16,23 @@ class ride:
         self.end_time = f_time
         self.ride_id = ids
         self.distance = (abs(self.end_x-self.start_x) + abs(self.end_y - self.start_y))
+
+
+testCars = list()
+testRides = list()
+def takeBestRide(x, y):
+    #najnizszy czas startu
+    #najniszy czas konca
+    #najmniejszy dystans
+    act_best_ride_id = testRides[0].ride_id
+    act_best_start = testRides[0].start_time
+    act_best_finish = testRides[0].end_time
+    act_best_distance_to_start = abs( x - testRides[0].start_x) + abs( y - testRides[0].start_y)
+    for element in testRides:
+        if element.start_time <= act_best_start:
+            if element.end_time <= act_best_finish:
+                if act_best_distance_to_start <= abs( x - element.start_x) + abs( y - element.start_y):
+                    act_best_ride_id = element.ride_id
+    return act_best_ride_id
+
 
